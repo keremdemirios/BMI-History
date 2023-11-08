8ce772cd7ddff3500426c00148d9342cb414780c
Kerem Demir <120031527+keremdemirios@users.noreply.github.com>
2023-11-08 15:10:31 +0300

    Initial Commit

---
+++ BMI History/BMI_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_HistoryApp.swift
+//  BMI History
+//
+//  Created by Kerem Demir on 8.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BMI_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
